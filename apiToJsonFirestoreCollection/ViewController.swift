c5ab646314d4c66a66378888cf263d66fdc612db
sasakazu <kazuya.sasakura@gmail.com>
2021-09-03 18:25:39 +0900

    Initial Commit

---
+++ apiToJsonFirestoreCollection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  apiToJsonFirestoreCollection
+//
+//  Created by 笹倉一也 on 2021/09/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
